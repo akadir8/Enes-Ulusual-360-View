cc852ec178c43dbdb613077189170aeec8ea4915
akadir8 <bickiabdulkadir12@gmail.com>
2023-07-27 19:49:16 +0300

    updated

---
+++ src/components/Banner.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {TypeAnimation} from 'react-type-animation'
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variant';
+
+function Banner() {
+  return (
+  <section className='section' id='home'>
+    <div className="container mx-auto">
+      <div>
+
+        <div>
+          <div>
+            <h1>Enes <span>Ulusal</span></h1>
+            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+             
+             <TypeAnimation sequence={[
+              '360° Çekim',2000,
+              'Fotoğraf',2000,
+             ]}
+             speed={50}
+             className='text-white'
+             wrapper='span'
+             repeat={Infinity}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+export default Banner;
