41b203e61b5b5747f5e9d476764ea1bc085a9bc6
akadir8 <bickiabdulkadir12@gmail.com>
2023-07-27 19:48:22 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,85 +1,22 @@
 import React from "react";
-import { Card } from "./components/Card";
-import { Navbar } from "./components/Navbar";
-import { Communication } from "./components/Communication";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Contact from "./components/Contact";
+import Nav from "./components/Nav";
+import About from "./components/About";
 
-const cardData = [
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "100px",
-    left: "40px"
-  },
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "100px",
-    left: "340px"
-  },
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "100px",
-    left: "640px"
-  },
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "100px",
-    left: "940px"
-  },
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "100px",
-    left: "1240px"
-  },
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "310px",
-    left: "40px"
-  },
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "310px",
-    left: "340px"
-  },
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "310px",
-    left: "640px"
-  },
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "310px",
-    left: "940px"
-  },
-  {
-    imageUrl: require("./components/görüntü.jpg"),
-    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
-    top: "310px",
-    left: "1240px"
-  },
-];
+
+
 
 function App() {
   return (
-    <div className="flex">
-      <Navbar />
-      {cardData.map((card, index) => (
-        <Card
-          key={index}
-          imageUrl={card.imageUrl}
-          iframeUrl={card.iframeUrl}
-          top={card.top}
-          left={card.left}
-        />
-      ))}
-      <Communication />
+    <div className="bg-no-repeat bg-cover overflow-hidden">
+      <Header/>
+      <Banner/>
+      <Nav/>
+      <About/>
+      <Contact/>
+      <div className="h-[250px]"></div>
     </div>
   );
 }
