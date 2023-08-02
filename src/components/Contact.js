e433c4465420ba4f0ff8917d01f9785e30a7f572
akadir8 <bickiabdulkadir12@gmail.com>
2023-08-02 18:20:54 +0300

    updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,42 @@
-//Bu sayfa 2 tane beyaz ekran olacak bir tarafında fotoğraf bir tarafın da sosyal medya hesapları olacak.
-
 import React from "react";
-import  { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+import İMG from "../assets/intro.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 
-function About() {
-    return (
-        <div className=" mx-auto bg-white w-[680px] h-[480px] section" id="about">
-            <div className="container mx-auto">
-                <div className="flex flex-col lg:flex-row items-center justify-between">
-                    <h3 className="bg-slate-400">Sosyal Medya Hesapları </h3>
-                    <div className="w-full lg:w-1/2"></div>
-                </div>
-            </div>
+function Contact() {
+  return (
+    <div className="social">
+      <div className="item">
+        <div className="contact">
+          <div className="first-text">Çekim Yapmaya Başlayalım Mı</div>
+          <img src={İMG} alt="" className="image" />
         </div>
-    )
+        <div className="social-media">
+          <div>
+            <h4 className="thrid-text">Sosyal Medya</h4>
+          </div>
+          <ul className="social-media-icon">
+            <li>
+              <FontAwesomeIcon icon={faTwitter} />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faFacebookF} />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faInstagram} />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default About;
+export default Contact;
