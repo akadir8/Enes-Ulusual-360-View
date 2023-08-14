6b41292f8024732c3a4c3401a8a27c949789201b
akadir8 <bickiabdulkadir12@gmail.com>
2023-08-14 14:59:21 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Contact from "./components/Contact";
 import Nav from "./components/Nav";
-import About from "./components/About";
+import Products from "./components/Products";
 
-
-
-
+const cardData = [
+  {
+    imageUrl: require("./assets/görüntü.jpg"),
+    iframeUrl: "https://kuula.co/share/collection/7FCcS?logo=1&info=0&logosize=143&fs=1&vr=0&gyro=0&initload=0&autorotate=0.27&autop=250&autopalt=1&thumbs=4&alpha=0.79&inst=0&keys=0",
+    top: "1000px",
+    left: "40px"
+  },
+]
 function App() {
+  
   return (
     <div className="bg-no-repeat bg-cover overflow-hidden">
-      <Header/>
-      <Banner/>
-      <Nav/>
-      <About/>
-      <Contact/>
+      <Header />
+      <Banner />
+      <Nav />
+      {cardData.map((card, index) => (
+        <Products
+          key={index}
+          imageUrl={card.imageUrl}
+          iframeUrl={card.iframeUrl}
+          top={card.top}
+          left={card.left}
+        />
+      ))}
+      <Contact />
       <div className="h-[20px]"></div>
     </div>
   );
