e7cd59277d55c89c79e9ea0ad7c87f1a455e8e93
akadir8 <bickiabdulkadir12@gmail.com>
2023-08-14 13:46:12 +0300

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Logo from '../assets/360.png'
+import { Link } from "react-scroll";
 
 function Header() {
   return (
     <div className='py-8'>
       <div className="container mx-auto">
         <div className='flex justify-between items-center'>
-          <a href="#">
             <img src={Logo} alt="" height={80} width={80} />
-          </a>
-          <button className='btn btn-sm'>Benimle Çalış</button>
+          <Link to='contact'
+          activeClass="active"
+          smooth={true}
+          spy={true}
+          offset={-200}
+          ><button className='btn btn-sm'>Benimle Çalış</button></Link>
         </div>
       </div>
     </div>
